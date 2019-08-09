17a3679c7d7418d0fdd59d16598244de84636e81
Matt Klaber <klaber+git@gmail.com>
2019-08-09 16:08:42 +0100

    Put the DSS lib creator in a script

--- make_dss_pip.sh
+++ make_dss_pip.sh
@@ -1,8 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+VERSION=$(curl -v --silent https://www.dataiku.com/dss/trynow/linux/ 2>&1 | grep -e 'var cur_version' | sed -n 's/^.*"\(.*\)".*$/\1/p')
+
+mkdir -p lib
+
+cd lib
+
+wget "https://cdn.downloads.dataiku.com/public/dss/$VERSION/dataiku-dss-$VERSION.tar.gz"
+
+tar -zxf "dataiku-dss-$VERSION.tar.gz"
+
+tee "dataiku-dss-$VERSION/python/setup.py" << END
 from setuptools import setup
 
 setup(
     name='dataiku',
-    version='1.0.0',
+    version='$VERSION',
     packages=['dataiku', 'dataiku.sql', 'dataiku.base', 'dataiku.core', 'dataiku.spark', 'dataiku.doctor',
               'dataiku.doctor.utils', 'dataiku.doctor.crossval', 'dataiku.doctor.clustering',
               'dataiku.doctor.prediction', 'dataiku.doctor.posttraining', 'dataiku.doctor.deep_learning',
@@ -12,9 +26,19 @@ setup(
               'dataiku.connector', 'dataiku.container', 'dataiku.runnables', 'dataiku.customstep', 'dataiku.fsprovider',
               'dataiku.customformat', 'dataiku.customrecipe', 'dataiku.customwebapp', 'dataiku.customtrigger',
               'dataikuapi', 'dataikuapi.dss', 'dataikuapi.apinode_admin'],
-    url='',
+    url='https://www.dataiku.com',
     license='',
     author='',
     author_email='',
     description=''
 )
+END
+
+cd "dataiku-dss-$VERSION/python"
+python setup.py -q sdist
+CUR_DIR=$(pwd)
+
+echo ""
+echo ">>>>>>>>>>> USE THIS PIP COMMAND <<<<<<<<<<<<<<<"
+echo ""
+echo "pip install dataiku --upgrade --no-index --find-links file://$CUR_DIR/dist"
